9a43b42d10da95f1cffc7abfa93a393eb0630799
beliemun <beliemun@paran.com>
2021-03-23 13:07:57 +0900

    update Amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import Router from "./Router";
 import GlobalStyle from "Components/GlobalStyles";
+// import Amplify, { Auth } from "aws-amplify";
+// import awsconfig from "./aws-exports";
+// Amplify.configure(awsconfig);
+
+// const username = "burngrit@icloud.com";
+// const password = "1234qwer";
+
+// Auth.signIn(username, password)
+//   .then((success) => console.log("successful sign in"))
+//   .catch((err) => console.log("err:", err));
 
 const App = () => (
   <>
