63252640af325c821b0b34256db993a3b6e70a94
beliemun <beliemun@paran.com>
2021-03-26 12:14:37 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Router from "./Router";
 import GlobalStyle from "Components/GlobalStyles";
-import Amplify, { Auth } from "aws-amplify";
-import awsconfig from "./aws-exports";
-Amplify.configure(awsconfig);
+// import Amplify, { Auth } from "aws-amplify";
+// import awsconfig from "./aws-exports";
+// Amplify.configure(awsconfig);
 
-const username = "burngrit@icloud.com";
-const password = "qwer1234";
-Auth.signIn(username, password)
-  .then((success) => console.log(success))
-  .catch((err) => console.log("err:", err));
+// const username = "burngrit@icloud.com";
+// const password = "qwer1234";
+// Auth.signIn(username, password)
+//   .then((success) => console.log(success))
+//   .catch((err) => console.log("err:", err));
 
 // Auth.signUp({ username, password })
 //   .then((success) => console.log("suc:", success))
