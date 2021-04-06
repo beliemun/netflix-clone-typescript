25994f1f0f1e5d18cb952f07ae22bfdd7ef273d6
beliemun <beliemun@paran.com>
2021-04-06 14:01:06 +0900

    Update fb.ts

--- src/fb.ts
+++ src/fb.ts
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,6 +15,7 @@ const firebaseConfig = {
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 }
 
 export const fb = firebase;
